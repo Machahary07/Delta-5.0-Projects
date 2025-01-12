17cf29744d84227f9907a3c1a849b072297b2430
Machahary07 <jeumachahary07@gmail.com>
2025-01-12 17:17:06 +0530

    `Added Qs6 JavaScript function to check if two numbers have the same last digit & ui for that`

--- Js/JS2 Qs6.js
+++ Js/JS2 Qs6.js
@@ -1,1 +1,16 @@
-// function
+//Qs6 (Bonus). WAP to check if 2 numbers have the same last digit. Eg: 32 and 47852 have the same last digit i.e. 2.
+function checkLastDigit() {
+    const num1 = document.getElementById('num1').value;
+    const num2 = document.getElementById('num2').value;
+    if (!num1 || !num2) {
+        document.getElementById('result').innerText = "Please enter both numbers.";
+        return;
+    }
+    const lastDigitNum1 = num1 % 10;
+    const lastDigitNum2 = num2 % 10;
+    if (lastDigitNum1 === lastDigitNum2) {
+        document.getElementById('result').innerText = `Yes, ${num1} and ${num2} have the same last digit.`;
+    } else {
+        document.getElementById('result').innerText = `No, ${num1} and ${num2} do not have the same last digit.`;
+    }
+}
--- Js/Questions.md
+++ Js/Questions.md
@@ -1,5 +1,5 @@
 Js pt 2 questions
-    Qs1. Create a number variablenum with some value. Now, print “good” if the number is divisible by 10 and print “bad” if it is not.
+    Qs1. Create a number variable num with some value. Now, print “good” if the number is divisible by 10 and print “bad” if it is not.
 
     Qs2. Take the user's name & age as input using prompts. Then return back the following statement to the user as an alert 
         (by substituting their name & age): name is age years old. [ Use template Literals to print thissentence ]
